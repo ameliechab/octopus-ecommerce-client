57bbf1f92548c020d0092b95745a001426a750f0
LucileTech <lucile.t.tech@gmail.com>
2022-12-07 15:33:54 +0100

    navBar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Navbar.css";
+import "./NavBar.css";
 import { Link, Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import useAuth from "../../auth/useAuth";
